062b92afc9975ba505b700609d071846224b489a
Eduardo Q <qe.eduardo99@gmail.com>
2023-04-10 15:25:12 -0600

    refactor: clean up comments from network analysis algorithm

--- 11_network_analysis.py
+++ 11_network_analysis.py
@@ -32,49 +32,33 @@ Path = List[int]
 
 def shortest_paths_from(from_user_id: int,
                         friendships: Friendships) -> Dict[int, List[Path]]:
-    # A dictionary from "user_id" to *all* shortest paths to that user
     shortest_paths_to: Dict[int, List[Path]] = {from_user_id: [[]]}
-
-    # A queue of (previous user, next user) that we need to check.
-    # Starts out with all pairs (from_user, friend_of_from_user)
     frontier = deque((from_user_id, friend_id)
                      for friend_id in friendships[from_user_id])
 
-    # Keep going until we empty the queue.
     while frontier:
-        # Remove the pair that's next in the queue.
         prev_user_id, user_id = frontier.popleft()
-
-        # Because of the way we're adding to the queue,
-        # necessarily we already know some shortest paths to prev_user
         paths_to_prev_user = shortest_paths_to[prev_user_id]
         new_paths_to_user = [path + [user_id] for path in paths_to_prev_user]
-
-        # It's possible we already know a shortest path to user_id.
         old_paths_to_user = shortest_paths_to.get(user_id, [])
-
-        # What's the shortest path to here that we've seen so far?
         if old_paths_to_user:
             min_path_length = len(old_paths_to_user[0])
         else:
             min_path_length = float('inf')
-
-        # Only keep paths that aren't too long and are actually new
+            
         new_paths_to_user = [path
                              for path in new_paths_to_user
                              if len(path) <= min_path_length
                              and path not in old_paths_to_user]
 
         shortest_paths_to[user_id] = old_paths_to_user + new_paths_to_user
-
-        # Add never-seen neighbors to the frontier
+        
         frontier.extend((user_id, friend_id)
                         for friend_id in friendships[user_id]
                         if friend_id not in shortest_paths_to)
 
     return shortest_paths_to
 
-# For each from_user, for each to_user, a list of shortest paths.
 shortest_paths = {user.id: shortest_paths_from(user.id, friendships)
                   for user in users}
 
@@ -82,9 +66,9 @@ betweenness_centrality = {user.id: 0.0 for user in users}
 
 for source in users:
     for target_id, paths in shortest_paths[source.id].items():
-        if source.id < target_id:      # don't double count
-            num_paths = len(paths)     # how many shortest paths?
-            contrib = 1 / num_paths    # contribution to centrality
+        if source.id < target_id:
+            num_paths = len(paths)
+            contrib = 1 / num_paths
             for path in paths:
                 for between_id in path:
                     if between_id not in [source.id, target_id]:
@@ -115,7 +99,7 @@ def matrix_times_vector(m: Matrix, v: Vector) -> Vector:
     n = len(v)
     assert nc == n, "must have (# of cols in m) == (# of elements in v)"
 
-    return [dot(row, v) for row in m]  # output has length nr
+    return [dot(row, v) for row in m]
 
 
 def find_eigenvector(m: Matrix,
@@ -123,12 +107,11 @@ def find_eigenvector(m: Matrix,
     guess = [random.random() for _ in m]
 
     while True:
-        result = matrix_times_vector(m, guess)    # transform guess
-        norm = magnitude(result)                  # compute norm
-        next_guess = [x / norm for x in result]   # rescale
+        result = matrix_times_vector(m, guess)
+        norm = magnitude(result)
+        next_guess = [x / norm for x in result]
 
         if distance(guess, next_guess) < tolerance:
-            # convergence so return (eigenvector, eigenvalue)
             return next_guess, norm
 
         guess = next_guess
@@ -157,21 +140,15 @@ def page_rank(users: List[User],
               endorsements: List[Tuple[int, int]],
               damping: float = 0.85,
               num_iters: int = 100) -> Dict[int, float]:
-    # Compute how many people each person endorses
     outgoing_counts = Counter(target for source, target in endorsements)
-
-    # Initially distribute PageRank evenly
     num_users = len(users)
     pr = {user.id : 1 / num_users for user in users}
-
-    # Small fraction of PageRank that each node gets each iteration
     base_pr = (1 - damping) / num_users
 
     for iter in tqdm.trange(num_iters):
-        next_pr = {user.id : base_pr for user in users}  # start with base_pr
+        next_pr = {user.id : base_pr for user in users}
 
         for source, target in endorsements:
-            # Add damped fraction of source pr to target
             next_pr[target] += damping * pr[source] / outgoing_counts[source]
 
         pr = next_pr
